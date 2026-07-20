79d5143a69e25acd0579eafd3a610276fd06235c
NAGESHWARI VIJAY GHONGADE <167419574+nageshwari21@users.noreply.github.com>
2026-07-20 12:14:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
-    allowedHosts: true,
-  },
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true
+  }
 })
